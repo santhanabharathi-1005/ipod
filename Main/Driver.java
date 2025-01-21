d0eb4276ecc72c7d7f0b97850eb87429fee22914
santhanabharathi p <130127142+santhanabharathi-1005@users.noreply.github.com>
2025-01-21 20:51:49 +0530

    Update Driver.java

--- Main/Driver.java
+++ Main/Driver.java
@@ -4,13 +4,12 @@ import com.ipod.implementation.*;
 public class Driver extends ObjectCreation{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ObjectCreation oc=new ObjectCreation();
 		Hyra H=oc.Create("Miles",17,"2.5");
 		H.Random();
 		Hyra iPod1 = oc.Create("Miles", 17, "2.5 hours");
         if (iPod1 != null) {
-           // iPod1.displayDetails();
+        
             iPod1.change(20);
             iPod1.power("On");
         }
